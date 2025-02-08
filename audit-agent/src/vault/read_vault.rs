7c1716afb2853ad36f23893cb4d942e16fe9a011
Jon <jsmaxi666@gmail.com>
2025-02-08 18:05:05 +0200

    Add shuttle toml to include vault folder, move vault folder

--- audit-agent/src/vault/read_vault.rs
+++ audit-agent/src/vault/read_vault.rs
@@ -1,8 +1,9 @@
 use crate::models::vulnerability::Vulnerability;
+use std::env;
 use std::panic::catch_unwind;
 use std::process::{Command, Output};
 
-const READ_SCRIPT_PATH: &str = "../vault/readReport.js";
+// const READ_SCRIPT_PATH: &str = "../vault/readReport.js";
 
 pub fn try_read_report_from_vault(id: &str) -> Option<Vec<Vulnerability>> {
     let result = catch_unwind(|| {
@@ -19,11 +20,22 @@ pub fn try_read_report_from_vault(id: &str) -> Option<Vec<Vulnerability>> {
 }
 
 fn read_report_from_vault(id: &str) -> Vec<Vulnerability> {
-    let output: Output = Command::new("node")
-        .arg(READ_SCRIPT_PATH)
-        .arg(&id)
-        .output()
-        .unwrap();
+    let path = format!(
+        "{}/vault/readReport.js",
+        env::var("CARGO_MANIFEST_DIR").unwrap()
+    );
+
+    println!("path {}", path);
+
+    let _output = Command::new("node").arg(path).arg(&id).output();
+
+    if _output.is_err() {
+        let error_string = _output.unwrap_err().to_string();
+        println!("Read error: {}", error_string);
+        panic!("Read error: {}", error_string);
+    }
+
+    let output: Output = _output.unwrap();
 
     if output.status.success() {
         let result: String = String::from_utf8(output.stdout).unwrap().trim().to_string();
--- audit-agent/src/vault/write_vault.rs
+++ audit-agent/src/vault/write_vault.rs
@@ -1,8 +1,9 @@
 use crate::models::report::VulnerabilityReport;
+use std::env;
 use std::panic::catch_unwind;
 use std::process::{Command, Output};
 
-const WRITE_SCRIPT_PATH: &str = "../vault/writeReport.js";
+// const WRITE_SCRIPT_PATH: &str = "../vault/writeReport.js";
 
 pub fn try_write_report_to_vault(report: &VulnerabilityReport) -> Option<String> {
     let result = catch_unwind(|| {
@@ -19,6 +20,13 @@ pub fn try_write_report_to_vault(report: &VulnerabilityReport) -> Option<String>
 }
 
 fn write_report_to_vault(report: &VulnerabilityReport) -> String {
+    let path = format!(
+        "{}/vault/writeReport.js",
+        env::var("CARGO_MANIFEST_DIR").unwrap()
+    );
+
+    println!("path {}", path);
+
     let input_json = serde_json::to_string(&report.vulnerabilities);
 
     if input_json.is_err() {
@@ -27,11 +35,15 @@ fn write_report_to_vault(report: &VulnerabilityReport) -> String {
 
     let input_json: String = input_json.unwrap();
 
-    let output: Output = Command::new("node")
-        .arg(WRITE_SCRIPT_PATH)
-        .arg(&input_json)
-        .output()
-        .unwrap();
+    let _output = Command::new("node").arg(path).arg(&input_json).output();
+
+    if _output.is_err() {
+        let error_string = _output.unwrap_err().to_string();
+        println!("Write error: {}", error_string);
+        panic!("Write error: {}", error_string);
+    }
+
+    let output: Output = _output.unwrap();
 
     if output.status.success() {
         let resuld_id: String = String::from_utf8(output.stdout).unwrap().trim().to_string();
